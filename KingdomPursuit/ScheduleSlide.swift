f05242d7939a40837c005b5f4f15a083e2fc8f51
Forrest Brown <forrestmax@icloud.com>
2021-03-12 13:32:56 -0500

    Functioning Schedule!!

--- KingdomPursuit/ScheduleSlide.swift
+++ KingdomPursuit/ScheduleSlide.swift
@@ -7,11 +7,66 @@
 
 import UIKit
 
-class ScheduleSlide: UITableView {
+class ScheduleSlide: UIView {
     
+    @IBOutlet var view:UIView!
+    @IBOutlet var scrollView:UIScrollView!
+    @IBOutlet var titleLabel:UILabel!
+    @IBOutlet var time1:UILabel!
+    @IBOutlet var time2:UILabel!
+    @IBOutlet var time3:UILabel!
+    @IBOutlet var time4:UILabel!
+    @IBOutlet var time5:UILabel!
+    @IBOutlet var time6:UILabel!
+    @IBOutlet var time7:UILabel!
+    @IBOutlet var time8:UILabel!
+    @IBOutlet var time9:UILabel!
+    @IBOutlet var time10:UILabel!
+    @IBOutlet var time11:UILabel!
+    @IBOutlet var time12:UILabel!
+    @IBOutlet var time13:UILabel!
+    @IBOutlet var time14:UILabel!
+    @IBOutlet var time15:UILabel!
+    @IBOutlet var event1:UILabel!
+    @IBOutlet var event2:UILabel!
+    @IBOutlet var event3:UILabel!
+    @IBOutlet var event4:UILabel!
+    @IBOutlet var event5:UILabel!
+    @IBOutlet var event6:UILabel!
+    @IBOutlet var event7:UILabel!
+    @IBOutlet var event8:UILabel!
+    @IBOutlet var event9:UILabel!
+    @IBOutlet var event10:UILabel!
+    @IBOutlet var event11:UILabel!
+    @IBOutlet var event12:UILabel!
+    @IBOutlet var event13:UILabel!
+    @IBOutlet var event14:UILabel!
+    @IBOutlet var event15:UILabel!
+    
+    public func configure(with events: Array<String>, times: Array<String>, name: String) {
+        
+        let timeLabels = [time1, time2, time3, time4, time5, time6, time7, time8, time9, time10, time11, time12, time13, time14, time15]
+        let eventLabels = [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12, event13, event14, event15]
+        
+        //Set the day title
+        titleLabel.text = name
+        
+        //Replace the text labels with the 'times' array
+        for i in 0 ..< events.count{
+            timeLabels[i]?.text = times[i]
+            eventLabels[i]?.text = events[i]
+        }
+        
+        //Delete any labels left after all of the times are processed.
+        for i in events.count ..< timeLabels.count{
+            timeLabels[i]?.removeFromSuperview()
+            eventLabels[i]?.removeFromSuperview()
+        }
+    }
+    
+    // Initialization code
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     
--- KingdomPursuit/ScheduleView.swift
+++ KingdomPursuit/ScheduleView.swift
@@ -13,6 +13,12 @@ class ScheduleView: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var pageControl: UIPageControl!
     
     var slides:[ScheduleSlide] = [];
+    let thursTimes = ["3:00pm","6:00pm","6:30pm - 9:15pm"]
+    let thursEvents = ["Registration opens","Auditorium opens","Session 1: BOB HAZLETT"]
+    let friTimes = ["8:00am","8:30am","9:00am - 10:45am","11:00am - 12:00pm","12:15pm - 1:30pm","1:30pm","2:00pm - 3:00pm","3:15pm - 4:15pm","4:15pm - 6:30pm","5:30pm","6:00pm","6:30pm - 9:00pm"]
+    let friEvents = ["Atrium opens","Auditorium opens","Session 2: BOB HAZLETT","Session 3: VAN COCHRANE","Prophetic Panel Lunch","Atrium opens", "Breakout 1","Breakout 2","Dinner break","Atrium opens","Auditorium opens","Session 4: WENDY BACKLUND"]
+    let satTimes = ["8:00am","8:30am","9:00am - 10:45am","11:00am = 12:00pm","12:00pm - 2:00pm","1:30pm","2:00pm - 3:00pm","3:15pm - 4:15pm", "4:15pm - 6:30pm", "5:30pm", "6:00pm","6:30pm - 9:00pm"]
+    let satEvents = ["Atrium opens","Auditorium opens","Session 5: PUTTY PUTMAN","Session 6: CLAY HARRINGTON","Lunch break","Atrium opens","Breakout 1","Breakout 2","Dinner break","Atrium opens","Auditorium opens","Session 7: PUTTY PUTMAN"]
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +35,13 @@ class ScheduleView: UIViewController, UIScrollViewDelegate {
     
     func createSlides() -> [ScheduleSlide] {
         let slide1:ScheduleSlide = Bundle.main.loadNibNamed("ScheduleSlide", owner: self, options: nil)?.first as! ScheduleSlide
-        slide1.backgroundColor = .red
+        slide1.configure(with: thursEvents, times: thursTimes, name: "Thursday Schedule")
                 
         let slide2:ScheduleSlide = Bundle.main.loadNibNamed("ScheduleSlide", owner: self, options: nil)?.first as! ScheduleSlide
-        slide2.backgroundColor = .yellow
+        slide2.configure(with: friEvents, times: friTimes, name: "Friday Schedule")
                 
         let slide3:ScheduleSlide = Bundle.main.loadNibNamed("ScheduleSlide", owner: self, options: nil)?.first as! ScheduleSlide
-        slide3.backgroundColor = .blue
+        slide3.configure(with: satEvents, times: satTimes, name: "Saturday Schedule")
         
         return [slide1, slide2, slide3]
     }
@@ -66,162 +72,3 @@ class ScheduleView: UIViewController, UIScrollViewDelegate {
         let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
     }
 }
-        
-//
-//        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
-//
-//        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
-//
-//            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
-//            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
-//
-//        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
-//            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
-//            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
-//
-//        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
-//            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
-//            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
-//
-//        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
-//            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
-//            slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
-//        }
-//    }
-    
-//    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-//        setupSlideScrollView(slides: slides)
-//    }
-//}
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 10
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell", for: indexPath)
-//        cell.textLabel?.text = "Hello world"
-//        return cell
-//    }
-//
-//    let thursTimes = [
-//        "3:00pm",
-//        "6:00pm",
-//        "6:30pm – 9:15pm"
-//    ]
-//
-//    let thursEvents = [
-//        "Registration opens",
-//        "Auditorium opens",
-//        "Session 1: BOB HAZLETT"
-//    ]
-//
-//    var thurs: UIView!
-//    var fri: UIView!
-//    var sat: UIView!
-//
-//    private let scrollView = UIScrollView()
-//
-//    private let pageControl: UIPageControl = {
-//        let pageControl = UIPageControl()
-//        pageControl.numberOfPages = 3
-//        pageControl.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
-//        return pageControl
-//    }()
-//
-//    func thursDesign(tableView: UITableView)
-//    {
-//        thurs.backgroundColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
-//
-//        thurs.addSubview(tableView)
-//    }
-//
-//    func friDesign(tableView: UITableView)
-//    {
-//        fri.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
-//        fri.addSubview(tableView)
-//    }
-//
-//    func satDesign(tableView: UITableView)
-//    {
-//        sat.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-//        sat.addSubview(tableView)
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        scrollView.delegate = self
-//        pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged)
-//        scrollView.backgroundColor = #colorLiteral(red: 0.6984269023, green: 1, blue: 0, alpha: 1)
-//        view.addSubview(scrollView)
-//        view.addSubview(pageControl)
-//    }
-//
-//    @objc private func pageControlDidChange(_ sender: UIPageControl){
-//        let current = sender.currentPage
-//        scrollView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width, y: 0), animated: true)
-//    }
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - 150, width: view.frame.size.width, height: 70)
-//
-//        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - 75)
-//
-//        if scrollView.subviews.count == 2 {
-//            configureScrollView()
-//        }
-//    }
-//    private func configureScrollView(){
-//        scrollView.contentSize = CGSize(width: view.frame.width * 3, height: scrollView.frame.height)
-//        scrollView.isPagingEnabled = true
-//
-//        for x in 0..<3{
-//            let tableView: UITableView = {
-//                let tableView = UITableView()
-//                tableView.backgroundColor = #colorLiteral(red: 0.7570065856, green: 0.7925702929, blue: 0.4755898118, alpha: 1)
-//                tableView.frame = view.bounds
-//                tableView.dataSource = self
-//                tableView.register(UITableViewCell.self, forCellReuseIdentifier: "scheduleCell")
-//                return tableView
-//            }()
-//
-//            let page = UIView(frame: CGRect(x: CGFloat(x) * view.frame.size.width, y: 0, width: view.frame.size.width, height: scrollView.frame.size.height))
-//
-//            if x == 0 {
-//                func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//                    let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell", for: indexPath)
-//                    cell.textLabel?.text = "Yoyo"
-//                    return cell
-//                }
-//                thurs = page
-//                thursDesign(tableView: tableView)
-//            }
-//            if x == 1 {
-//                fri = page
-//                friDesign(tableView: tableView)
-//            }
-//            if x == 2 {
-//                sat = page
-//                satDesign(tableView: tableView)
-//            }
-//            scrollView.addSubview(page)
-//        }
-//    }
-//}
-//
-//extension ScheduleView: UIScrollViewDelegate {
-//    func scrollViewDidScroll(_ scrollView:UIScrollView) {
-//        pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x) / Float(scrollView.frame.size.width)))
-//    }
-//}
-
-//extension ScheduleView: UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 10
-//    }
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell", for: indexPath)
-//        cell.textLabel?.text = "Yoyo"
-//        return cell
-//    }
-//}
